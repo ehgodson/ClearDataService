f0ef8e6748867d851da8ed05798c3e6231bfa645
Godwin Ehichoya <ehgodson@hotmail.com>
2025-03-04 06:07:50 +0000

    Enhance IDataService with new methods and updates
    - Modified IDataService to include new methods for adding, deleting, and updating entities, with a new `trackEntities` parameter.
    - Updated project version from 2.0.0 to 2.0.1 and package versions for Microsoft.Data.SqlClient and EntityFrameworkCore.
    - Implemented new functionality in BaseDataService to support the updated IDataService interface.
    - Added static method `AddDataService` for easier registration in the service collection.
    - Improved comments and documentation for better maintainability.

--- Abstractions/IDataService.cs
+++ Abstractions/IDataService.cs
@@ -4,13 +4,14 @@ public interface IDataService
 {
     Task<T?> Get<T>(int id) where T : class;
     Task<T?> Get<T>(string id) where T : class;
-    Task<T?> Get<T>(Expression<Func<T, bool>> predicate) where T : class;
     Task<List<T>> Get<T>(bool trackEntities = false) where T : class;
+    Task<T?> Get<T>(Expression<Func<T, bool>> predicate, bool trackEntities = true) where T : class;
+    Task<T?> GetOne<T>(bool trackEntities = false) where T : class;
     Task<List<T>> Find<T>(Expression<Func<T, bool>> predicate, bool trackEntities = false) where T : class;
 
     DbSet<T> GetEntity<T>() where T : class;
-    IQueryable<T> GetAsQueryable<T>() where T : class;
-    IQueryable<T> FindAsQueryable<T>(Expression<Func<T, bool>> predicate) where T : class;
+    IQueryable<T> GetAsQueryable<T>(bool trackEntities = false) where T : class;
+    IQueryable<T> FindAsQueryable<T>(Expression<Func<T, bool>> predicate, bool trackEntities = false) where T : class;
 
 
     int Count<T>() where T : class;
@@ -26,6 +27,15 @@ public interface IDataService
     Task<int> Delete<T>(IEnumerable<T> entities) where T : class;
 
 
+    void AddForInsert<T>(T entity) where T : class;
+    void AddAllForInsert<T>(IEnumerable<T> entities) where T : class;
+    void AddForDelete<T>(T entity) where T : class;
+    void AddAllForDelete<T>(IEnumerable<T> entities) where T : class;
+    void AddForUpdate<T>(T entity) where T : class;
+    void AddAllForUpdate<T>(IEnumerable<T> entities) where T : class;
+    Task<int> SaveChanges();
+
+
     //Task<List<T>> FromSql<T>(string sql) where T : class;
     Task<int> ExecuteSql(string sql);
     Task<int> ExecuteSql(string sql, params object[] parameters);
@@ -36,13 +46,4 @@ public interface IDataService
     Task<T?> QueryFirstOrDefault<T>(string sql);
     Task<T?> QueryFirstOrDefault<T>(string sql, object parameters);
     //Task<int> ExecuteQuery(string sql);
-
-
-    void AddForInsert<T>(T entity) where T : class;
-    void AddAllForInsert<T>(IEnumerable<T> entities) where T : class;
-    void AddForDelete<T>(T entity) where T : class;
-    void AddAllForDelete<T>(IEnumerable<T> entities) where T : class;
-    void AddForUpdate<T>(T entity) where T : class;
-    void AddAllForUpdate<T>(IEnumerable<T> entities) where T : class;
-    Task<int> SaveChanges();
 }
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,30 @@
 # Changelog
 
+## [2.0.1] - 2025-01-31
+### Changed
+- IDataService 
+	- Added GetOne
+	- Added tracking option to Get
+	- Added tracking option to GetAsQueryable
+	- Added tracking option to FindAsQueryable
+- Updated packages
+
+## [2.0.0] - 2025-01-17
+### Added
+- IEntity
+- IEntityRepo
+- BaseEntityRepo
+- BaseDataService
+- DataService
+- DataServiceMiddlewareExtension
+### Removed
+- IGenericService
+- AbstractGenericService
+- AbstractDataService
+### Changed
+- IDataService
+- Updated packages
+
 ## [1.0.3] - 2024-09-07
 ### Changed
 - IDataService
